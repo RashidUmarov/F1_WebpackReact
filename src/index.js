1dc3a75985bbe3d2733f04916f8887e7d6752d21
Rashid Umarov <rasrasras2014@mail.ru>
2023-06-04 16:19:50 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+//console.log('Hello, World');
+
+ReactDOM.render(<App/>, document.getElementById("root"));
