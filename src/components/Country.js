eea1fed6e121561bc7004569083d1d354ed3fea8
Rashid Umarov <rasrasras2014@mail.ru>
2023-06-05 14:37:34 +0300

    added list of countries

---
+++ src/components/Country.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import Button from 'react-bootstrap/Button';
+
+import "../styles/Country.css";
+
+function Country(props){
+    const [selected, changeSelected] = useState(false);
+
+    return(
+        <tr className={selected ? "selected-country":""}>
+            <td>{props.country.name}</td>
+            <td>{props.country.capital}</td>
+            <td>
+                {selected ?
+                <Button variant="danger" onClick={()=> changeSelected(false)}>Remove</Button>:
+                <Button variant="success" onClick={()=> changeSelected(true)}>Add</Button>
+                }
+            </td>
+        </tr>
+    )
+}
+
+export default Country;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,25 @@
-import React, { Component } from "react";
-import Header from "./Header";
+import React from "react";
 import "../styles/Main.css";
+import Countries from "./Countries";
 
-class Main extends Component{
-    render(){
-        return(
-            <main>
-                <div>
-                    <h1> Hello World APP</h1>
-                </div>
-            </main>
-        )
-    }
+function Main(){
+    return(
+        <main>
+               <Countries/>
+        </main>
+    )
 }
+
+//class Main extends Component{
+//    render(){
+//        return(
+//            <main>
+//                <div>
+//                    <h1> Hello World APP</h1>
+//                </div>
+//            </main>
+//        )
+//    }
+//}
 
 export default Main;
